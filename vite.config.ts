370c16cda6261ba5dc9432f3de88ff79c16f67b8
stg42895 <stg42895@gmail.com>
2025-07-01 12:32:57 +0530

    Configure app for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/LuckyPick/',
   plugins: [
     react(),
     VitePWA({
@@ -45,8 +45,8 @@ export default defineConfig({
         background_color: '#ffffff',
         display: 'standalone',
         orientation: 'portrait',
-        scope: '/',
-        start_url: '/',
+        scope: '/LuckyPick/',
+        start_url: '/LuckyPick/',
         icons: [
           {
             src: 'icon-192x192.svg',
