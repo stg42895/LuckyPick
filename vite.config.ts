e6910c0859785d0c438339d35e26e10322c3fef8
stg42895 <stg42895@gmail.com>
2025-07-01 11:53:23 +0530

    Fix PWA Icon Build Errors

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/luckypick-lottery/',
   plugins: [
     react(),
     VitePWA({
@@ -47,18 +48,18 @@ export default defineConfig({
         background_color: '#ffffff',
         display: 'standalone',
         orientation: 'portrait',
-        scope: '/',
-        start_url: '/',
+        scope: '/luckypick-lottery/',
+        start_url: '/luckypick-lottery/',
         icons: [
           {
-            src: 'icon-192x192.png',
+            src: 'icon-192x192.svg',
             sizes: '192x192',
-            type: 'image/png'
+            type: 'image/svg+xml'
           },
           {
-            src: 'icon-512x512.png',
+            src: 'icon-512x512.svg',
             sizes: '512x512',
-            type: 'image/png'
+            type: 'image/svg+xml'
           }
         ]
       }
