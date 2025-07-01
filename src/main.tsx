dbf84613283a0508bcc8b58e2177bedc30c427c8
stg42895 <stg42895@gmail.com>
2025-07-01 11:31:19 +0530

    Fix service worker process.env error

--- src/main.tsx
+++ src/main.tsx
@@ -2,20 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { register } from './utils/serviceWorker'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
-
-// Register service worker for PWA functionality
-register({
-  onSuccess: () => {
-    console.log('PWA installed successfully');
-  },
-  onUpdate: () => {
-    console.log('PWA updated');
-  }
-});
